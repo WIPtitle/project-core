4a009fed3866169853b1b78e791712ebaf0cc720
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-11-01 11:47:39 +0100

    feat: implemented auth on ntfy

---
+++ other-entrypoints/ntfy_entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/ash
+
+apk add openssl
+
+CREDENTIALS_FILE=${NTFY_CREDENTIALS_FILE}
+
+echo "Searching Ntfy credentials in ${NTFY_CREDENTIALS_FILE}"
+
+if [ ! -f "$CREDENTIALS_FILE" ]; then
+    # if credential file does not exist, delete other files since it is a fresh installation
+    [ -f /var/lib/ntfy/cache.db ] && rm /var/lib/ntfy/cache.db
+    [ -f /var/lib/ntfy/auth.db ] && rm /var/lib/ntfy/auth.db
+
+    echo "No credentials found, generating new credentials..."
+    NTFY_WRITER_PASSWORD=$(openssl rand -base64 32)
+    NTFY_READER_PASSWORD=$(openssl rand -base64 32)
+    cat <<EOF > "$CREDENTIALS_FILE"
+{
+  "NTFY_TOPIC": "alarm-ntfy-topic",
+  "NTFY_WRITER_USER": "alarm-ntfy-writer-user",
+  "NTFY_WRITER_PASSWORD": "$NTFY_WRITER_PASSWORD",
+  "NTFY_READER_USER": "alarm-ntfy-reader-user",
+  "NTFY_READER_PASSWORD": "$NTFY_READER_PASSWORD"
+}
+EOF
+fi
+
+NTFY_TOPIC=$(awk -F'"' '/NTFY_TOPIC/ {print $4}' "$CREDENTIALS_FILE")
+NTFY_WRITER_USER=$(awk -F'"' '/NTFY_WRITER_USER/ {print $4}' "$CREDENTIALS_FILE")
+NTFY_WRITER_PASSWORD=$(awk -F'"' '/NTFY_WRITER_PASSWORD/ {print $4}' "$CREDENTIALS_FILE")
+NTFY_READER_USER=$(awk -F'"' '/NTFY_READER_USER/ {print $4}' "$CREDENTIALS_FILE")
+NTFY_READER_PASSWORD=$(awk -F'"' '/NTFY_READER_PASSWORD/ {print $4}' "$CREDENTIALS_FILE")
+
+export NTFY_BASE_URL=http://localhost:8080
+export NTFY_CACHE_FILE=/var/lib/ntfy/cache.db
+export NTFY_CACHE_DURATION=336h
+export NTFY_AUTH_FILE=/var/lib/ntfy/auth.db
+export NTFY_AUTH_DEFAULT_ACCESS=deny-all
+export NTFY_BEHIND_PROXY=true
+export NTFY_ATTACHMENT_CACHE_DIR=/var/lib/ntfy/attachments
+export NTFY_ATTACHMENT_EXPIRY_DURATION=336h
+export NTFY_ENABLE_LOGIN=true
+
+[ ! -f "$NTFY_CACHE_FILE" ] && touch "$NTFY_CACHE_FILE"
+[ ! -f "$NTFY_AUTH_FILE" ] && touch "$NTFY_AUTH_FILE"
+
+ntfy serve &
+
+NTFY_PASSWORD="$NTFY_WRITER_PASSWORD" ntfy user add "$NTFY_WRITER_USER"
+NTFY_PASSWORD="$NTFY_READER_PASSWORD" ntfy user add "$NTFY_READER_USER"
+
+ntfy access "$NTFY_WRITER_USER" "$NTFY_TOPIC" write-only
+ntfy access "$NTFY_READER_USER" "$NTFY_TOPIC" read-only
+
+tail -f /dev/null
