0c0398d696dbd33bb414819b657b425fe9391d1f
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-11-10 18:05:02 +0100

    feat: init push notification

--- other-entrypoints/ntfy_entrypoint.sh
+++ other-entrypoints/ntfy_entrypoint.sh
@@ -45,6 +45,8 @@ EOF
   export NTFY_ATTACHMENT_EXPIRY_DURATION=336h
   export NTFY_ENABLE_LOGIN=true
   export NTFY_POLL_INTERVAL=30
+  export NTFY_CORS_ENABLED=true
+  export NTFY_CORS_ORIGINS=*
 
   [ ! -f "$NTFY_CACHE_FILE" ] && touch "$NTFY_CACHE_FILE"
   [ ! -f "$NTFY_AUTH_FILE" ] && touch "$NTFY_AUTH_FILE"
